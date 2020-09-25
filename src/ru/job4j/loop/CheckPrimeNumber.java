ec8be4e4efc26f0e0bfae66cc6f21adf94d95ee2
hasover <hasover@gmail.com>
2020-09-25 21:20:08 +0300

    5.5. Простое число [#369850]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,11 +2,8 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
+        boolean prime = number > 1;
 
-        if (number == 1) {
-            prime = false;
-        }
         for (int i = 2; i <= number - 1; i++) {
             if (number % i == 0) {
                 prime = false;
