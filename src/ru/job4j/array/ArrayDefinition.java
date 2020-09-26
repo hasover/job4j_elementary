cb92096d85ca506e8b9e075e9d51a5659c5be8f1
hasover <hasover@gmail.com>
2020-09-26 19:54:26 +0300

    6.0.2. Заполнение массива. [#369772]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,17 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Alex Jones";
+        names[3] = "Tom Stanley";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+
+        }
+
     }
 }
