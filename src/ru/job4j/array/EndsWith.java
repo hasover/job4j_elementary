09aead361674d0d9dc18fb0e676d1877034d5c37
hasover <hasover@gmail.com>
2020-09-26 22:54:36 +0300

    6.4.2. Слово заканчивается на ... [#369764]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        /* проверить. что массив word имеет последние элементы одинаковые с post */
+        int i;
+        for (i = 0; i < post.length; i++) {
+            if (i >= word.length || word[word.length - 1 - i] != post[post.length - 1 - i]) {
+                break;
+            }
+        }
+
+        return i == post.length;
+    }
+}
