f571320e32838148d44a1c6cfaf4e7162365f510
hasover <hasover@gmail.com>
2020-09-27 14:22:44 +0300

    6.4. Слово начинается с ... [#369760]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         int i;
         for (i = 0; i < pref.length; i++) {
-            if (i >= word.length || word[i] != pref[i]) {
+            if (word[i] != pref[i]) {
                 break;
             }
         }
